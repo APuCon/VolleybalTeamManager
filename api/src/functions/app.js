eaae88910b727b20908c400d63ba345f40241160
APuCon <120116021+APuCon@users.noreply.github.com>
2026-09-22 20:18:59 +0200

    Add import for deletions module

--- api/src/functions/app.js
+++ api/src/functions/app.js
@@ -4,6 +4,7 @@ const { app } = require('@azure/functions');
 const { TableClient } = require('@azure/data-tables');
 require('./attendance');
 require('./substitutions');
+require('./deletions');
 
 const crypto = require('crypto');
 
